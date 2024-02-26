3c894d250b34b8f1b495dc09fa35df001d594120
MPutnis <martins.putnis@gmail.com>
2024-02-26 11:59:30 +0200

    Changed loop that displays Monday status in to a function.

--- part3.php
+++ part3.php
@@ -17,28 +17,33 @@ function checkFreeMonday(int $year)
     ($weekEnd == 0 || $weekEnd == 6)? $isWeekend = true : $isWeekend = false;
     return $isWeekend;
 }
-//loop that checks multiple years with the function
-$curDate = date("Y-m-d");
-echo $curDate , "<br>";
-for( $i = 2020; $i < 2031; $i++)
+
+function displayMonday( int $start, int $end )
 {
-    $date = date("Y-m-d",strtotime("$i-11-18")); // date formated for comparison
-    $nov = date("F d, Y", strtotime("$i-11-18")); // date formated for output
-    
-    echo "The next Monday after $nov ";
-    if (checkFreeMonday($i))
-    {    
-        if ($curDate > $date) //past
-            echo "was";
-        else //future
-            echo "will be";
-    } 
-    else 
+    $curDate = date("Y-m-d");
+    echo $curDate , "<br>";
+    for( $i = $start; $i <= $end; $i++)
     {
-        if ($curDate > $date) //past
-            echo "was not";
-        else //future
-            echo "will not be";
+        $date = date("Y-m-d",strtotime("$i-11-18")); // date formated for comparison
+        $nov = date("F d, Y", strtotime("$i-11-18")); // date formated for output
+        
+        echo "The next Monday after $nov ";
+        if (checkFreeMonday($i))
+        {    
+            if ($curDate > $date) //past
+                echo "was";
+            else //future
+                echo "will be";
+        } 
+        else 
+        {
+            if ($curDate > $date) //past
+                echo "was not";
+            else //future
+                echo "will not be";
+        }
+        echo " a holiday. <br>";
     }
-    echo " a holiday. <br>";
 }
+
+//displayMonday(2020, 2030); //commented out so it wouldn't display in part4_result.php
