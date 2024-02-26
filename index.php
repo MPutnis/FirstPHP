261ae8658c089c6eee806be0af58cfb84cc8a03b
MPutnis <martins.putnis@gmail.com>
2024-02-26 10:38:26 +0200

    Links in index.php are now in new lines.

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
     <?php 
         echo "Hello from php <br>"; 
     ?>
-    <a href="/part2.php"> part2 </a>
-    <a href="/part3.php"> part3 </a>
-    <a href="/part4_input.php"> part4_input </a>
+    <a href="/part2.php"> part2 </a><br>
+    <a href="/part3.php"> part3 </a><br>
+    <a href="/part4_input.php"> part4_input </a><br>
 </body>
 
