3c7435ba29c45fdb9e485b6b63ed9219d9ca49f2
MPutnis <martins.putnis@gmail.com>
2024-02-26 12:00:32 +0200

    Turned the interval output in to a function call from part3.php.

--- part4_result.php
+++ part4_result.php
@@ -1,4 +1,5 @@
 <?php
+require_once('part3.php');
 $yearStart = $yearStartErr =  $yearEnd = $yearEndErr = $intervalErr = "";
 
 // validate inputs
@@ -26,22 +27,6 @@ function validYear( $year)
     return $isValid;
 }
 
-//function that takes year as input checks monday
-function checkFreeMonday(int $year)
-{
-    //check if nov. 18 of that year was sat or sun
-    $Y = $year;
-    $M = 11;
-    $d = 18;
-    $date = strtotime("$Y-$M-$d");
-    $weekEnd = date('w', $date);
-    //echo $weekEnd;
-    $isWeekend = false;
-    //return true if it was, false if wasn't
-    ($weekEnd == 0 || $weekEnd == 6)? $isWeekend = true : $isWeekend = false;
-    return $isWeekend;
-}
-
 //error messeges
 if ($yearStartErr != "")
     echo $yearStartErr, "<br>";
@@ -52,31 +37,6 @@ elseif ($intervalErr != "")
 
 // interval output
 else
-{
-    $curDate = date("Y-m-d");
-    //loop that checks multiple years with the function
-    for( $i = $yearStart; $i <= $yearEnd; $i++)
-    {
-        $date = date("Y-m-d",strtotime("$i-11-18")); // date formated for comparison
-        $nov = date("F d, Y", strtotime("$i-11-18")); // date formated for output
-        
-        echo "The next Monday after $nov ";
-        if (checkFreeMonday($i))
-        {    
-            if ($curDate > $date) //past
-                echo "was";
-            else //future
-                echo "will be";
-        } 
-        else 
-        {
-            if ($curDate > $date) //past
-                echo "was not";
-            else //future
-                echo "will not be";
-        }
-        echo " a holiday. <br>";
-    }
-}
+    displayMonday($yearStart, $yearEnd); // function from part3.php
 ?>
 <a href="/part4_input.php">Back to form</a>
